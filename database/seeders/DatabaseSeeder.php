fa2e52c9fd2d450e3eab556bf9d66abcef755420
Your Name <you@example.com>
2026-06-17 14:02:37 +0300

    Добавлены новые сидеры для базы данных

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         User::factory(10)->create();
 
-//        User::query()->create([
-//            'name' => 'Admin',
-//            'email' => 'admin@mail.ru',
-//            'password' => Hash::make('qwerty12345'),
-//            'surname' => 'Admin',
-//            'phone' => '+7(999)999-99-99',
-//            'role_id' => '1',
-//        ]);
-//
+        User::query()->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.ru',
+            'password' => Hash::make('qwerty12345'),
+            'surname' => 'Admin',
+            'phone' => '+7(999)999-99-99',
+            'role_id' => '1',
+        ]);
+
         $this->call(RoleSeeder::class);
     }
 }
