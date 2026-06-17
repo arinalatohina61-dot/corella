ca911abade71929f5771879cf5f58c850dd2cac3
Your Name <you@example.com>
2026-06-17 13:59:51 +0300

    Добавлены новые сидеры для базы данных

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::query()->create([
+            'name' => 'admin',
+        ]);
+
+        Role::query()->create([
+            'name' => 'manager',
+        ]);
+
+        Role::query()->create([
+            'name' => 'client',
+        ]);
+    }
+}
